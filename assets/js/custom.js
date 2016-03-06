0ba58444eaf60aed7aa0e41fd0ca0a4717c6c769
Jim O'Neill <jim.oneill@oliverwyman.com>
2016-03-05 23:33:29 -0500

    Fleshing out lots of JS documentation in comments

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,53 +1,62 @@
-/*==========================================================
+/*
+============================================================
     
-    Custom JavaScript 
+    Custom JavaScript for Parsley Helpers Demo
 
-=========================================================*/
+============================================================
+*/
+
+/**
+ * The scripts below provide an example of how to use the functions
+ * from `parsley-helpers.js` to add dynamic, conditional areas to a form 
+ * that's validated via Parsley.
+ */
 
 $(document).ready(function () {
     
-    // Bind form to Parsley for validation, 
-    // and add custom validator functions
-    // ------------------------------------
+    /*  Parsley setup
+    --------------------------------------*/
     
+    /**
+     * Bind form to Parsley for validation, with our customized options,
+     * then add the custom validator functions we want.
+     */
     $('#myForm').parsley(parsleyOptions);
     addCustomValidators();
     
     
-    // Manage showing/hiding form sections
-    // -----------------------------------
+    /*  Form fields setup
+    --------------------------------------*/
     
-    // Disable hidden fields right off the bat
+    /**
+     * Right away, disable form fields inside any container that's hidden. 
+     */
+    disableInputsWithin('.' + hiddenClass);
     
-    disableInputsWithin('.hidden');
     
-    // Also disable fields that are controlled by a toggle switch
+    /*  Show/hide conditional areas
+    --------------------------------------*/
     
-    $('[data-toggled-by]').each(function() {
-        var antecedent = $(this).attr('data-toggled-by');
-        if ($(antecedent).not(':checked')) {
-            disableInputsWithin(this);
-            $(this).addClass('do-not-enable');
-        }
-    });
+    /**
+     * The basic pattern we use for this feature consists of:
+     * 1. A form field with multiple options (such as a <select> or radio 
+     *    button group).
+     * 2. One or more other elements in the DOM whose display state 
+     *    (shown/hidden) depends on which of #1's options is selected.
+     * 
+     * Note below that to identify the group of DOM elements tied to each 
+     * option, we use custom `data-` attributes in the markup, such as 
+     * `[data-rel-payment-method="creditcard"]`. This may feel verbose and 
+     * awkward, but it makes more semantic sense than using CSS classes to 
+     * specify this information.
+     */
     
-    // When a toggle switch changes, toggle its target
-    
-    $(document).on('change', 'input:radio', function () {        
-        $('[data-toggle-target]').filter(':not(.hidden *)').each(function() {
-            var target = $(this).attr('data-toggle-target');
-            if ($(this).is(':checked')) {
-                enableInputsWithin(target);
-                $(target).removeClass('do-not-enable');
-            } else {
-                disableInputsWithin(target);
-                $(target).addClass('do-not-enable');
-            }
-        });
-    });
-    
-    // Payment Method
-    
+    /**
+     * Payment Method toggle
+     * 
+     * When the value of the Payment Method <select> menu changes, show the 
+     * content related to either Credit Card or eCheck.
+     */
     $(document).on('change', '#selectPaymentMethod', function () {
         var cc = '[data-rel-payment-method="creditcard"]';
         var ec = '[data-rel-payment-method="echeck"]';
@@ -63,8 +72,12 @@ $(document).ready(function () {
         }
     });
     
-    // Payment Type
-    
+    /**
+     * Payment Type toggle
+     * 
+     * When the value of the Payment Type radio button group changes, 
+     * show the content related to either One-time or Recurring.
+     */
     $(document).on('change', 'input:radio[name="optionsPaymentType"]', function () {
         var ot = '[data-rel-payment-type="onetime"]';
         var rc = '[data-rel-payment-type="recurring"]';
@@ -81,27 +94,110 @@ $(document).ready(function () {
     });
     
     
-    // Button actions
-    // --------------
+    /*  Enable/disable fields via toggle switches
+    -----------------------------------------------*/
     
+    /**
+     * This feature demonstrates a slightly different approach than the basic 
+     * conditional-areas show/hide behavior.  
+     * 
+     * The expected pattern is something like:
+     * 1. A group of radio options.
+     * 2. One or more of the radio options contains an associated <input> or 
+     *    <select> whose value is required if its parent option is checked.
+     *
+     * Example: the question "How did you hear about us?" might have 3 radio 
+     * options: Advertisement, Word of Mouth, or Other. If Other is checked, 
+     * the user must fill out a nearby text input explaining their answer.
+     * 
+     * To work with the rest of our Parsley behavior, and provide a good user 
+     * experience, the target (input or select) is always [disabled] unless its 
+     * toggle switch (radio option) is checked.
+     * 
+     * We don't specify a hierarchical relationship between the toggle switch 
+     * and its target - i.e. the target does not need to be a child or a sibling 
+     * of the toggle switch.
+     * 
+     * Instead, we use a custom `data-toggle-target` attribute on the toggle 
+     * switch, whose value is a selector that matches the target; and we use a 
+     * custom `data-toggled-by` attribute on the target, whose value is a 
+     * selector that matches the toggle switch. (We could get by with just one 
+     * of these attributes and not the other, but it's more performant and 
+     * useful to have both.)
+     */
+    
+    /**
+     * Right away, disable any fields that are targets of a toggle switch
+     * (unless the toggle switch happens to be checked).
+     */
+    $('[data-toggled-by]').each(function() {
+        var antecedent = $(this).attr('data-toggled-by');
+        if ($(antecedent).not(':checked')) {
+            disableInputsWithin(this);
+            $(this).addClass('do-not-enable');
+        }
+    });
+    
+    /**
+     * When a toggle switch changes, toggle its target.
+     * 
+     * Note that we listen for a change on *all* radio buttons to make this work, 
+     * since scoping to only those with `[data-toggle-target]` will only capture 
+     * a change when those elements become checked, not when they become 
+     * un-checked.
+     * 
+     * Two other things to note, which make this work with our other Parsley 
+     * behavior:
+     * 1. We filter our selection of objects to eliminate any that are children 
+     *    of a 'hidden' container - anything that's hidden is irrelevant.
+     * 2. We also manually add/remove the '.do-not-enable' class on the target 
+     *    field whenever it becomes disabled/enabled; this sets a flag that 
+     *    allows us to ignore this field if we need to hide/show its parent 
+     *    form section. (See documentation for `enableInputsWithin()` in 
+     *    `parsley-helpers.js`.)   
+     */
+    $(document).on('change', 'input:radio', function () {        
+        $('[data-toggle-target]').filter(':not(.' + hiddenClass + ' *)').each(function() {
+            var target = $(this).attr('data-toggle-target');
+            if ($(this).is(':checked')) {
+                enableInputsWithin(target);
+                $(target).removeClass('do-not-enable');
+            } else {
+                disableInputsWithin(target);
+                $(target).addClass('do-not-enable');
+            }
+        });
+    });
+
+    
+    /*  Button actions
+    --------------------------------------*/
+    
+    /**
+     * The Submit button uses Parsley to validate the form.
+     */
     $('#btnSubmitPayment').on('click', function (evt) {
         evt.preventDefault();
         validateForm('#myForm', function () {
             // On success
-            $('#formStep1').addClass('hidden');
-            $('#formStep2').removeClass('hidden');
+            // Hide step 1, show step 2
+            $('#formStep1').addClass(hiddenClass);
+            $('#formStep2').removeClass(hiddenClass);
         }, function () {
             // On error
-            // Nothing beyond Parsley's default behavior
+            // Do nothing beyond Parsley's default behavior
         });
     });
     
+    /**
+     * The Back button in Step 2 just toggles the form back to its initial 
+     * state, to make the demo easier to use. Note that it does not clear 
+     * the contents of any form fields.
+     */
     $('#btnBack').on('click', function (evt) {
         evt.preventDefault();
-        $('#formStep2').addClass('hidden');
-        $('#formStep1').removeClass('hidden');
-    });
-    
+        $('#formStep2').addClass(hiddenClass);
+        $('#formStep1').removeClass(hiddenClass);
+    }); 
     
 });
-// The End
--- assets/js/parsley-helpers.js
+++ assets/js/parsley-helpers.js
@@ -1,91 +1,29 @@
-/*==========================================================
+/*
+============================================================
     
     Parsley Helper Functions
 
-=========================================================*/
+============================================================
+*/
 
-// Standardize Parsley initialization options
-// ------------------------------------------
+/*
+------------------------------------------------------------
+    1. Parsley setup & validation
+------------------------------------------------------------
+*/
 
-// We add '[disabled]' to the list of exclusions, so that we can
-// show/hide fields dynamically and disable their child fields, 
-// which allows Parsley to work properly.
-
-var parsleyOptions = {
-    excluded: "input[type=button], input[type=submit], input[type=reset], input[type=hidden], [disabled]"
-};
-
-
-// Utilities for enabling/disabling fields
-// ---------------------------------------
-
-// When form fields get shown/hidden dynamically, Parsley needs to be reset
-// in order for validation to work properly.
-
-function resetParsley(form) { 
-    $(form).parsley().destroy();
-    $(form).parsley(parsleyOptions);
-}
-
-// The set of form field objects we want to enable/disable should be 
-// the same as Parsley's included objects, with the added fact that
-// we only need to care about those that are [required]. 
-
-var incInputs = $(Parsley.options.inputs).not(Parsley.options.excluded).filter('[required]');
-
-// When we disable inputs, we don't care about special cases.
-// But when we enable inputs, we sometimes need to care, so we can 
-// optionally check for a `.do-not-enable` flag on the input or any ancestors.
-
-function disableInputsWithin(obj) {
-    $(obj).find(incInputs).prop('disabled', true);
-    resetParsley($(obj).closest('form'));
-}
-
-function enableInputsWithin(obj, filter) {
-    var inputs = $(obj).find(incInputs);
-    if (filter) {
-        inputs = inputs.filter(':not(.do-not-enable):not(.do-not-enable *)');
-    }
-    inputs.prop('disabled', false);
-    resetParsley($(obj).closest('form'));
-}
-
-// Hiding/showing form sections is a matter of:
-// 1. Toggling the container's visibility
-// 2. Toggling enabled/disabled on all form field children,
-//    but leaving any .do-not-enable fields alone.
-//
-// Note that these are built to accommodate objects that
-// consist of multiple form sections, not necessarily just one.
-
-function hideFormSection(sections) {
-    var s = $(sections);
-    s.each(function() {
-        $(this).addClass('hidden');
-        disableInputsWithin($(this));
-    });
-}
-
-function showFormSection(sections) {
-    var s = $(sections);
-    s.each(function() {
-        $(this).removeClass('hidden');
-        enableInputsWithin($(this), true);
-    });
-}
-
-// In most cases we'll use both Hide and Show together
-
-function hideShowFormSections(x, y) {
-    hideFormSection(x);
-    showFormSection(y);
-}
-
-
-// Define all custom Parsley validator functions
-// ---------------------------------------------
-
+/**
+ * Custom Parsley validator functions
+ * 
+ * Must be added after Parsley is initialized. These validate text strings for 
+ * proper formatting of the following types, which are not included with Parsley
+ * out of the box:
+ * 
+ * Date (US):       e.g. 4/29/15 or 4-29-15
+ * Phone:           e.g. (617) 555-1234 or 617-555-1234
+ * Zip (US):        e.g. 02043 or 02043-1961
+ * Credit Card:     e.g. 4444111111111111
+ */
 function addCustomValidators() {
     window.Parsley.addValidator('date', {
         requirementType: 'string',
@@ -94,8 +32,7 @@ function addCustomValidators() {
             return reGoodDate.test(value);
         },
         messages: {
-            en: 'Value must be a valid date.',
-            es: 'El valor debe ser una fecha válida.'
+            en: 'Value must be a valid date.'
         }
     }).addValidator('phone', {
         requirementType: 'string',
@@ -104,8 +41,7 @@ function addCustomValidators() {
             return reGoodPhNum.test(value);
         },
         messages: {
-            en: 'Phone numbers must be in valid 10-digit format, e.g. 123-456-7890.',
-            es: 'Los números de teléfono deben estar en formato de 10 dígitos válido, por ejemplo: 123-456-7890.'
+            en: 'Phone numbers must be in valid 10-digit format, e.g. 123-456-7890.'
         }
     }).addValidator('zip', {
         requirementType: 'string',
@@ -114,8 +50,7 @@ function addCustomValidators() {
             return reGoodZip.test(value);
         },
         messages: {
-            en: 'Enter a 5- or 9-digit zip code.',
-            es: 'Código postal debe ser 5 o 9 dígitos.'
+            en: 'Enter a 5- or 9-digit zip code.'
         }
     }).addValidator('creditcard', {
         requirementType: 'string',
@@ -124,15 +59,18 @@ function addCustomValidators() {
             return reGoodCC.test(value);
         },
         messages: {
-            en: 'Value must be a valid credit card number.',
-            es: 'El valor debe ser un número de tarjeta de crédito válida.'
+            en: 'Value must be a valid credit card number.'
         }
     });
 }
 
-// Validate the form and run success/error callbacks
-// -------------------------------------------------
-
+/**
+ * Validate the form itself
+ * 
+ * Allows a success callback function (needed if you want the form to do 
+ * something), and an error callback function (often not needed since Parsley's
+ * default behavior prevents submission and displays error messages).
+ */  
 function validateForm(form, success, error) {
     var f = $(form);
     f.parsley().validate();
@@ -142,4 +80,154 @@ function validateForm(form, success, error) {
     } else {
         if (typeof error === "function") { error(); }
     }
+}
+
+
+
+
+
+/*
+------------------------------------------------------------
+    2. Parsley with dynamically shown/hidden fieldsets
+------------------------------------------------------------
+*/
+
+/**
+ * Once Parsley has been initialized, it keeps track of all fields marked 
+ * [required]. So if a required field gets hidden via user action, Parsley 
+ * will still think it's required and invalidate the form.
+ * 
+ * To prevent this situation, we do several things:
+ *
+ * 1. Configure Parsley's options so that it will ignore fields that are 
+ *    [disabled], even if they are marked [required].
+ * 2. Ensure that every time we hide a set of form fields, we also disable 
+ *    all fields in that set (and vice versa when we re-show it). 
+ * 3. Re-initialize Parsley every time a change happens that hides or shows 
+ *    a set of form fields, to make sure it requires only the ones shown.
+ */
+
+/*  Custom Parsley initialization
+--------------------------------------*/
+
+/** 
+ * Adds '[disabled]' to the list of excluded selectors, per #1 above.
+ */
+var parsleyOptions = {
+    excluded: "input[type=button], input[type=submit], input[type=reset], input[type=hidden], [disabled]"
+};
+
+
+/*  Customizable utility selectors
+---------------------------------------------*/
+
+/** 
+ * These selectors are used in the functions below; you can replace them 
+ * with your own preferred alternatives. 
+ *
+ * hiddenClass: CSS class used to hide content, usually via display: none. This 
+ * must be defined in your stylesheet. Leave off the initial '.'
+ */
+var hiddenClass = 'hidden';
+ 
+/**
+ * doNotEnableSelector: A selector that acts as a flag to prevent the enabling 
+ * of a form field, if it should stay disabled.
+ */
+var doNotEnableSelector = '.do-not-enable';
+
+
+/*  Resetting Parsley
+---------------------------------------------*/
+
+/** 
+ * Function for re-initializing Parsley, per #3 above.
+ * 
+ * Note that we need to re-initialize using our customized options, to make 
+ * sure the [disabled] behavior still works.
+ */
+function resetParsley(form) { 
+    $(form).parsley().destroy();
+    $(form).parsley(parsleyOptions);
+}
+
+
+/*  Enabling/disabling form fields 
+---------------------------------------------*/
+
+/**
+ * Custom collection of form field objects to enable/disable
+ * 
+ * The set of form field objects we want to act on should be the same as 
+ * Parsley's included objects, with the added fact that we only need to care 
+ * about those that are [required]. 
+ */
+var incInputs = $(Parsley.options.inputs).not(Parsley.options.excluded).filter('[required]');
+
+/**
+ * When disabling fields, set the `disabled` property, then reset Parsley.
+ * 
+ * Note that this function (and its counterpart below) targets the *parent* of 
+ * the form field(s), not the field(s) themselves, because 98% of the time 
+ * we want to disable a set of fields at once, not just one. This means that 
+ * any field you want to enable/disable must have a targetable parent.
+ */
+function disableInputsWithin(obj) {
+    $(obj).find(incInputs).prop('disabled', true);
+    resetParsley($(obj).closest('form'));
+}
+
+/**
+ * When enabling fields, we need to be careful, since some fields may be 
+ * disabled for an unrelated reason (and should stay that way). So we optionally
+ * check for a `do not enable` flag on each field and its ancestors, then set 
+ * the `disabled` property, then reset Parsley.
+ */
+function enableInputsWithin(obj, filter) {
+    var inputs = $(obj).find(incInputs);
+    if (filter) {
+        inputs = inputs.filter(':not(' + doNotEnableSelector + '):not(' + doNotEnableSelector + ' *)');
+    }
+    inputs.prop('disabled', false);
+    resetParsley($(obj).closest('form'));
+}
+
+
+/*  Hiding/showing form sections 
+---------------------------------------------*/
+
+/** 
+ * Hiding/showing form sections consists of:
+ * 1. Toggling the section containers' visibility
+ * 2. Using our functions above to enable/disable all form field children,
+ *    but leave any `do not enable` fields alone.
+ * 
+ * Note that these are built to accommodate objects that
+ * consist of multiple form sections, not necessarily just one.
+ */
+function hideFormSection(sections) {
+    var s = $(sections);
+    s.each(function() {
+        $(this).addClass(hiddenClass);
+        disableInputsWithin($(this));
+    });
+}
+
+function showFormSection(sections) {
+    var s = $(sections);
+    s.each(function() {
+        $(this).removeClass(hiddenClass);
+        enableInputsWithin($(this), true);
+    });
+}
+
+/**
+ * In most cases we'll use both Hide and Show together, so here they are.
+ * 
+ * The order is important, because you want to be able to, for example, 
+ * hide *all* form sections, then show just the one you want.
+ */
+function hideShowFormSections(x, y) {
+    hideFormSection(x);
+    showFormSection(y);
 }
